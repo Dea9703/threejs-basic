b24b5534a56d268bc54dc731e874f4547db7c906
dea9703 <dea9703@163.com>
2022-11-20 20:19:06 +0800

    feat: 添加路由

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,8 +1,17 @@
 <template>
-  <div />
+  <div>
+    <el-button type="primary" size="large" @click="fetchLogin">login</el-button>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { login } from '@/api/user'
+
+const fetchLogin = async () => {
+  const data = {}
+  const res = await login(data)
+  console.log(res)
+}
 
 </script>
 
